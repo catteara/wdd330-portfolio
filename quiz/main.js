ce847d01e1ae1d1265d5867915d2db0b5d23224d
Catteara <catteara@gmail.com>
2021-05-05 19:12:43 -0700

    10

--- quiz/main.js
+++ quiz/main.js
@@ -1,69 +1,66 @@
 const quiz = [
-    ["What is Superman's real name?","Clark Kent"],
-    ["What is Wonder Woman's real name?","Diana Prince"],
-    ["What is Batman's real name?","Bruce Wayne"]
-];
+    { name: "Superman",realName: "Clark Kent" },
+    { name: "Wonderwoman",realName: "Dianna Prince" },
+    { name: "Batman",realName: "Bruce Wayne" },
+  ];
 
-function start(quiz){
-let score = 0;
+// View Object
+const view = {
+score: document.querySelector('#score strong'),
+question: document.getElementById('question'),
+result: document.getElementById('result'),
+info: document.getElementById('info'),
+start: document.getElementById('start'),
+render(target,content,attributes) {
+for(const key in attributes) {
+target.setAttribute(key, attributes[key]);
+}
+target.innerHTML = content;
+},
+show(element){
+element.style.display = 'block';
+},
+hide(element){
+element.style.display = 'none';
+}
+};
 
+// Game Object
+const game = {
+start(quiz){
+this.score = 0;
+this.questions = [...quiz];
+view.hide(view.start);
 // main game loop
-for(const [question,answer] of quiz){
-    const response = ask(question);
-    check(response,answer);
+for(const question of this.questions){
+this.question = question;
+this.ask();
 }
 // end of main game loop
-
-gameOver();
-
-// function declarations
-function ask(question){
-    return prompt(question);
+this.gameOver();
+},
+ask(){
+const question = `What is ${this.question.name}'s real name?`;
+view.render(view.question,question);
+const response =  prompt(question);
+this.check(response);
+},
+check(response){
+const answer = this.question.realName;
+if(response === answer){
+view.render(view.result,'Correct!',{'class':'correct'});
+alert('Correct!');
+this.score++;
+view.render(view.score,this.score);
+} else {
+view.render(view.result,`Wrong! The correct answer was ${answer}`,{'class':'wrong'});
+alert(`Wrong! The correct answer was ${answer}`);
+}
+},
+gameOver(){
+view.render(view.info,`Game Over, you scored ${this.score} point${this.score !== 1 ? 's' : ''}`);
+view.show(view.start);
+}
 }
 
-function check(response,answer){
-    if(response === answer){
-    alert('Correct!');
-    score++;
-    } else {
-    alert(`Wrong! The correct answer was ${answer}`);
-    }
-}
-
-function gameOver(){
-    alert(`Game Over, you scored ${score} point${score !== 1 ? 's' : ''}`);
-}
-}
-start(quiz);
-
-/* I commented all of this out, because in Ch4, it says to replace all of the code with Ch4's coding.
-//Ch1
-alert('Welcome to Quiz Ninja!');
-
-//Ch3
-const quiz = [
-    ["What is Superman's real name?","Clark Kent"],
-    ["What is Wonder Woman's real name?","Diana Prince"],
-    ["What is Batman's real name?","Bruce Wayne"]
-];
-
-let score = 0 // initialize score
-
-for(const [question,answer] of quiz){
-    const response = prompt(question);
-    if(response === answer){
-        alert('Correct!');
-        score++;
-    } else {
-        alert(`Wrong! The correct answer was ${answer}`);
-    }
-    }
-
-// At the end of the game, report the player's score
-alert(`Game Over, you scored ${score} point${score !== 1 ? 's' : ''}`);
-
-//Ch2
-const question = "What is Superman's real name?"
-const answer = prompt(question);
-alert(`You answered ${answer}`);
-*/
+view.start.addEventListener('click', () => game.start(quiz), false);
