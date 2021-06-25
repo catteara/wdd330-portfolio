5fc5894dfc15d3f8f5a78abffeeb54d046b138c6
Catteara <catteara@gmail.com>
2021-06-24 21:58:39 -0700

    33

--- week10/earthquake/index.js
+++ week10/earthquake/index.js
@@ -1,5 +1,5 @@
-import { getJSON, getLocation } from './utilities.js';
+import QuakesController from './QuakesController.js';
+import buildNavigation from './routing.js';
 
-const baseUrl ='https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2019-01-01&endtime=2019-02-02';
-
-//Import controller here, instantiate class, then call init
+const navElement = document.getElementById('mainNav');
+buildNavigation(navElement);
