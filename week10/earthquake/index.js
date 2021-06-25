57e7fe4bd29bdca7ab3f8123dcf558d201b23644
Catteara <catteara@gmail.com>
2021-06-24 22:13:11 -0700

    34

--- week10/earthquake/index.js
+++
@@ -1,5 +0,0 @@
-import QuakesController from './QuakesController.js';
-import buildNavigation from './routing.js';
-
-const navElement = document.getElementById('mainNav');
-buildNavigation(navElement);
