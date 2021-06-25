bf4ba5023c9dbe144335c3ac8d5c2232e5c5d45a
Catteara <catteara@gmail.com>
2021-06-24 21:48:19 -0700

    32

--- week10/earthquake/Quake.js
+++ week10/earthquake/Quake.js
@@ -1,9 +1,9 @@
-import { getJSON } from 'utilities.js';
+import { getJSON } from './utilities.js';
 // Quake Model
 export default class Quake {
   constructor() {
     this.baseUrl =
-      'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2019-01-01&endtime=2019-03-02';
+      'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson';
     // this is where we will store the last batch of retrieved quakes in the model.  I don't always do this...in this case the api doesn't have an endpoint to request one quake.
     this._quakes = [];
   }
--- week10/earthquake/index.js
+++ week10/earthquake/index.js
@@ -1,7 +1,5 @@
-import { getJSON, getLocation } from './utilities';
+import { getJSON, getLocation } from './utilities.js';
 
 const baseUrl ='https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2019-01-01&endtime=2019-02-02';
 
-// navigator.geolocation.getCurrentPosition((position)=>
-
-// )
+//Import controller here, instantiate class, then call init
